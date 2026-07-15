3cec49ea38ca70377d9480241389650aea0fced0
manikandan thangavel <sukumarthillairajan@gmail.com>
2026-07-15 19:42:50 +0530

    fix: removed the boilerplate App.jsx, made the main.jsx into the root entry point and App.jsx to be the main routing component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import Login from "./features/auth/pages/Login.jsx";
+import Register from "./features/auth/pages/Register.jsx";
+import Dashboard from "./features/problems/pages/Dashboard.jsx";
+import Arena from "./features/problems/pages/Arena.jsx";
+import SubmissionsPage from "./features/submissions/pages/SubmissionsPage.jsx";
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-blue-500">
-        Online Judge UI
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/problems/:id" element={<Arena />} />
+        <Route path="/problems/:id/submissions" element={<SubmissionsPage />} />
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from "./App.jsx";
-
-import Login from "./features/auth/pages/Login.jsx";
-import Register from "./features/auth/pages/Register.jsx";
-import Dashboard from "./features/problems/pages/Dashboard.jsx";
-import Arena from "./features/problems/pages/Arena.jsx";
-import SubmissionsPage from "./features/submissions/pages/SubmissionsPage.jsx";
 
 import "./index.css";
 
@@ -17,18 +10,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/problems/:id" element={<Arena />} />
-          <Route path="/problems/:id/submissions" element={<SubmissionsPage />}/>
-          {/* Catch-all route to redirect unknown URLs to home */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
+      <App />
     </QueryClientProvider>
   </StrictMode>,
 );
